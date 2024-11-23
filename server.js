06c42ce4e5a9ab79427f984ecb109fcbbb9659de
Hiroshi Fujii <guilhermefujii643@gmail.com>
2024-11-22 22:26:11 -0300

    Third commit

--- server.js
+++ server.js
@@ -6,5 +6,5 @@ app.listen(3000, () => {
 });
 
 app.get("/api", (req, res) => {
-    res.status(200).send("Boas Vindas à imersão!");
+    res.status(200).send("A Torre Eiffel iluminada à noite, com milhares de luzes cintilando, criando um espetáculo mágico em Paris.");
 });
